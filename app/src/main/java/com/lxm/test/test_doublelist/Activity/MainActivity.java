68e2db5c01bd625be98e6746b2b884c9a7b5990a
unknown <Administrator@SC-201607302202.(none)>
2016-08-21 00:13:28 +0800

    Initial commit

---
+++ app/src/main/java/com/lxm/test/test_doublelist/Activity/MainActivity.java
@@ -0,0 +1,132 @@
+package com.lxm.test.test_doublelist.Activity;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.lxm.test.test_doublelist.Adapter.LeftListAdapter;
+import com.lxm.test.test_doublelist.Adapter.MainSectionedAdapter;
+import com.lxm.test.test_doublelist.R;
+import com.lxm.test.test_doublelist.View.PinnedHeaderListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.left_listview)
+    ListView leftListview;
+    @Bind(R.id.pinnedListView)
+    PinnedHeaderListView pinnedListView;
+    private boolean isScroll = true;
+    private LeftListAdapter adapter;
+
+    private String[] leftStr = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+    private boolean[] flagArray = {true, false, false, false, false, false, false, false, false};
+    private String[][] rightStr = new String[][]{{"1", "1", "1"},
+            {"2", "2", "2"},
+            {"3", "3", "3"}, {"4", "4", "4"}, {"5", "5", "5"},
+            {"6", "6", "6"}, {"7", "7"},
+            {"8", "8", "8", "8", "8"}, {"9", "9", "9", " 9", "9"}
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        pinnedListView = (PinnedHeaderListView) findViewById(R.id.pinnedListView);
+        final MainSectionedAdapter sectionedAdapter = new MainSectionedAdapter(this, leftStr, rightStr);
+        pinnedListView.setAdapter(sectionedAdapter);
+        adapter = new LeftListAdapter(this, leftStr, flagArray);
+        leftListview.setAdapter(adapter);
+        leftListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                isScroll = false;
+
+                for (int i = 0; i < leftStr.length; i++) {
+                    if (i == position) {
+                        flagArray[i] = true;
+                    } else {
+                        flagArray[i] = false;
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                int rightSection = 0;
+                for (int i = 0; i < position; i++) {
+                    rightSection += sectionedAdapter.getCountForSection(i) + 1;
+                }
+                pinnedListView.setSelection(rightSection);
+            }
+        });
+
+        pinnedListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                switch (scrollState) {
+                    // 当不滚动时
+                    case  AbsListView.OnScrollListener.SCROLL_STATE_IDLE:
+                        // 判断滚动到底部
+                        if (pinnedListView.getLastVisiblePosition() == (pinnedListView.getCount() - 1)){
+                            leftListview.setSelection(ListView.FOCUS_DOWN);
+                        }
+                        // 判断滚动到顶部
+                        if (pinnedListView.getFirstVisiblePosition() == 0) {
+                            leftListview.setSelection(0);
+                        }
+
+                        break;
+                }
+            }
+
+            int y = 0;
+            int x = 0;
+            int z = 0;
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (isScroll) {
+                    for (int i = 0; i < rightStr.length; i++) {
+                        if (i == sectionedAdapter.getSectionForPosition(pinnedListView.getFirstVisiblePosition())) {
+                            flagArray[i] = true;
+                            x = i;
+                        } else {
+                            flagArray[i] = false;
+                        }
+                    }
+                    if (x != y) {
+                        adapter.notifyDataSetChanged();
+                        y = x;
+                        if (y == leftListview.getLastVisiblePosition()) {
+//                            z = z + 3;
+                            leftListview.setSelection(z);
+                        }
+                        if (x == leftListview.getFirstVisiblePosition()) {
+//                            z = z - 1;
+                            leftListview.setSelection(z);
+                        }
+                        if (firstVisibleItem + visibleItemCount == totalItemCount - 1) {
+                            leftListview.setSelection(ListView.FOCUS_DOWN);
+                        }
+                    }
+                } else {
+                    isScroll = true;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+}
+
+
+
+
